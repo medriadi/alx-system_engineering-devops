80287d27d72ebca43cfe7975d47a2a0b446a68b2
medriadi <mohamedriadi@outlook.com>
2024-05-21 13:55:31 +0100

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             employee_id = int(sys.argv[1])
-
+            
             # Fetch employee data
             user_response = requests.get(
                 f'{REST_API}/users/{employee_id}')
@@ -26,25 +26,26 @@ if __name__ == '__main__':
                 print(
                     f"Error fetching user data for employee ID {employee_id}")
                 sys.exit(1)
+
             user_data = user_response.json()
             emp_name = user_data.get('name')
 
-            # Fetch todos for the specific employee
+            # Fetch all todos and filter for the specific employee
             todos_response = requests.get(
                 f'{REST_API}/todos?userId={employee_id}')
             if todos_response.status_code != 200:
                 print(
                     f"Error fetching TODO data for employee ID {employee_id}")
                 sys.exit(1)
+
             todos_data = todos_response.json()
             total_tasks = len(todos_data)
             completed_tasks = [
                 task for task in todos_data if task.get('completed')]
 
             # Display the results
-            print(
-                f'Employee {emp_name} is done with tasks '
-                f'({len(completed_tasks)}/{total_tasks}):')
+            print(f'Employee {emp_name} is done with tasks
+                  ({len(completed_tasks)}/{total_tasks}):')
             for task in completed_tasks:
                 print(f'\t {task.get("title")}')
         else:
