590b4d456f90b616c595eb077ff0a40917432f89
medriadi <mohamedriadi@outlook.com>
2024-06-06 16:25:31 +0100

    Refactor code to retrieve number of subscribers from a Reddit subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""Write a Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
 
 import requests
 
-url = 'https://www.reddit.com/r/{}/about.json'
-
 
 def number_of_subscribers(subreddit):
-    '''get subscribers'''
-    header = {'user-agent': 'tabbykatz-app0'}
-    r = requests.get(url.format(subreddit), headers=header)
-    if r.status_code != 200:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-    return r.json().get("data", {}).get("subscribers", 0)
